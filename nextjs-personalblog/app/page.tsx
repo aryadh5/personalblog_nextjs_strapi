a914061915f3fdc24e15c73b98bccc0e815b3e63
aryadh5 <aryadifah@gmail.com>
2024-10-26 23:13:38 +0700

    first commit

---
+++ nextjs-personalblog/app/page.tsx
@@ -0,0 +1,10 @@
+import Categories from "@/components/Categories";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Categories />
+    </div>
+  );
+}
